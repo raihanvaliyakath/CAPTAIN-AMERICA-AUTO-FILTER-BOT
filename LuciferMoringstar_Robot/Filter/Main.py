7d92940389710d775adb154af9fd42216d98308d
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2021-10-28 21:35:17 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -99,7 +99,7 @@ async def filter(client, message):
                 await message.reply_photo(photo=poster, caption=mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text(mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=https://telegra.ph/file/0671faf4ded8d944de89f.jpg, caption=mo_tech_yt, reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
