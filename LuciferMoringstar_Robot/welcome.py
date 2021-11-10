167b227b5b3f96020f1458139ab0fa36ec1de0de
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2021-11-10 18:42:18 +0530

    Update welcome.py

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -13,5 +13,6 @@ async def welcome(bot,message):
 async def goodbye(bot,message):
 	chatid= message.chat.id
 	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+
 	
 
