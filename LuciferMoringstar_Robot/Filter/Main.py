7c3162e4e144c8a6a17275b08583a41eec12c776
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2021-11-10 17:07:00 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -122,6 +122,8 @@ async def filter(client, message):
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
+    title = message.text
+    movie_info = get_movie_info(title) 
     if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
         return
     if 2 < len(message.text) < 50:    
