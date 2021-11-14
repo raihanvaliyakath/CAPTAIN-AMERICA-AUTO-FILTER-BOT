423d1633f4ab457e5c2b881b7eba58cf09eb728d
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2021-11-14 16:08:30 +0530

    Update welcome.py

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -8,17 +8,17 @@ from pyrogram.types import Message, User
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid= message.chat.id
-    Night_king=await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+    LuciferMoringstar=await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
         await asyncio.sleep(60) # in seconds
-        await Night_king.delete()
+        await LuciferMoringstar.delete()
     else:
         await msg.delete()
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
         chatid= message.chat.id
-    Night_king=await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+    LuciferMoringstar=await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
         await asyncio.sleep(60) # in seconds
-        await Night_king.delete()
+        await LuciferMoringstar.delete()
     else:
         await msg.delete()
 	
