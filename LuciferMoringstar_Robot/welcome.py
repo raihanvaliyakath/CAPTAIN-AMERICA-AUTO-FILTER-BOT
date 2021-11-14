a80306204a0bfd8c01076aa30f45d48a705b2573
Ragnarok <87938973+Ragnarok360@users.noreply.github.com>
2021-11-14 16:20:38 +0530

    Update welcome.py

--- LuciferMoringstar_Robot/welcome.py
+++ LuciferMoringstar_Robot/welcome.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, User
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid= message.chat.id
-        await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.title} ,  Happy to have here",chat_id=chatid)
+        await bot.send_message(text=f"Welcome {message.from_user.mention} to @{message.chat.username} ,  Happy to have here",chat_id=chatid)
         
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
